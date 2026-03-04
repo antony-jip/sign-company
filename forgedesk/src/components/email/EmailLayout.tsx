31934b6b2e4f8408cbc2bf4e15d9d13e221ed899
Antony Bootsma <antonybootsma@MacBook-Pro-van-Antony.local>
2026-03-04 21:24:23 +0100

    fix: email send typescript error

--- forgedesk/src/components/email/EmailLayout.tsx
+++ forgedesk/src/components/email/EmailLayout.tsx
@@ -755,8 +755,8 @@ export function EmailLayout() {
     const isScheduled = !!data.scheduledAt
 
     try {
-      await sendEmailViaApi(data.to, data.subject, data.body, {
-        scheduledAt: data.scheduledAt,
+      await sendEmail(data.to, data.subject, data.body, {
+        html: data.body,
       })
 
       const newEmail: Omit<Email, 'id' | 'created_at'> = {
